bdc297476773bdc8854b4152a2b405a260bbfe60
Эльдар <pikap.02@mail.ru>
2018-11-01 05:39:07 +0600

    Interfaces

---
+++ m3uDownload.DLL/Interfaces.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace m3uDownload.DLL
+{
+    public interface ISong
+    {
+        string Performer { get; set; }
+        string Title     { get; set; }
+        string Number    { get; set; }
+    }
+
+    public interface IWebSong : ISong
+    {
+        string WebLink   { get; set; }
+    }
+
+    public interface ILocalSong : ISong
+    {
+        string LocalDir  { get; set; }
+    }
+}
