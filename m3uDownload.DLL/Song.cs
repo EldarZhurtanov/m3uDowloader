e2ca576957d48e212daeb08493124ba1005e8eb5
Эльдар <pikap.02@mail.ru>
2018-11-01 05:47:39 +0600

    class Song

---
+++ m3uDownload.DLL/Song.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace m3uDownload.DLL
+{
+    public class Song : IWebSong, ILocalSong
+    {
+
+        public string Performer { get; set; }
+        public string Title     { get; set; }
+        public string Number    { get; set; }
+        public string WebLink   { get; set; }
+        public string LocalDir  { get; set; }
+
+        public Song(string performer, string title, string number, string webLink = null, string localDir = null)
+        {
+            Performer = performer;
+            Title = title;
+            Number = number;
+            WebLink = webLink;
+            LocalDir = localDir;
+        }
+    }
+}
