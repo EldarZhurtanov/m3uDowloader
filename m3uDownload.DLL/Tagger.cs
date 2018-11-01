43a987e41101f22a09856863e142610a24be7476
Эльдар <pikap.02@mail.ru>
2018-11-01 13:10:01 +0600

    class Tagger

---
+++ m3uDownload.DLL/Tagger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace m3uDownload.DLL
+{
+    public static class Tagger
+    {
+        public static void TagFile(ILocalSong song)
+        {
+            if (System.IO.File.Exists(song.LocalPath))
+            {
+                var tag = TagLib.File.Create(song.LocalPath);
+                tag.Tag.Performers = null;
+                tag.Tag.Performers = new string[1];
+                tag.Tag.Performers[0] = song.Performer;
+                tag.Tag.Title = song.Title;
+                tag.Tag.Track = Convert.ToUInt32(song.Number);
+                tag.Save();
+            }
+            else
+                throw new FileNotFoundException("Трек по пути \"" + song.LocalPath + "\" не найден!");
+        }
+    }
+}
--- m3uDownload.DLL/m3uParser.cs
+++ m3uDownload.DLL/m3uParser.cs
@@ -1,16 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.IO;
-using System.Threading.Tasks;
 using M3U.NET;
 
 namespace m3uDownload.DLL
 {
-    public class m3uParser
+    public static class m3uParser
     {
-        public IEnumerable<IWebSong> Parse(string m3uPlaylistPath)
+        public static IEnumerable<IWebSong> Parse(string m3uPlaylistPath)
         {
             if (File.Exists(m3uPlaylistPath))
             {
@@ -30,7 +27,7 @@ namespace m3uDownload.DLL
                 throw new FileNotFoundException("Указанный m3u - плейлист не существует!");
         }
 
-        private IWebSong ParseMediaItem(MediaItem item)
+        private static IWebSong ParseMediaItem(MediaItem item)
         {
             string[] separator = new string [] { " - " };
             string[] splited = item.Inf.Split(separator, 2, StringSplitOptions.RemoveEmptyEntries);
